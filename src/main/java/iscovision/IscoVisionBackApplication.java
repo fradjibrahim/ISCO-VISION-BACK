b09a6a230a17bf274ef1b37d02a58327815f42b6
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2024-01-02 23:30:04 -0500

    first commit

---
+++ src/main/java/iscovision/IscoVisionBackApplication.java
@@ -0,0 +1,13 @@
+package iscovision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IscoVisionBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IscoVisionBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/iscovision/IscoVisionBackApplicationTests.java
@@ -0,0 +1,13 @@
+package iscovision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IscoVisionBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
