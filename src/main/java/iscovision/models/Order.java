d327ec152a52580b9926d096a441bc61d7098c7d
ismaelcoulibaly <ismael.coulibaly99@gmail.com>
2024-01-02 23:26:48 -0500

    creation of models

---
+++ src/main/java/iscovision/models/Order.java
@@ -0,0 +1,13 @@
+package iscovision.models;
+
+import java.util.Date;
+
+public class Order {
+
+
+    private Long id;
+    private Date dateOfOrder;
+    private User user;
+
+
+}
---
+++ src/main/java/iscovision/models/User.java
@@ -0,0 +1,83 @@
+package iscovision.models;
+
+import java.util.List;
+
+public class User {
+
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address;
+
+    private List<Order> orders;
+
+
+    public User(Long id, String firstName, String lastName, String email, String phone, String address, List<Order> orders) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.orders = orders;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+}
